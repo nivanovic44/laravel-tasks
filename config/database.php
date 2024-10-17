f9524fe0ab568c5b777917763182a8e326b269b5
nivanovic44 <nikola.ivanovic@edu.sait.ca>
2024-10-17 16:39:19 -0600

    Configure DB & Redis variables

--- config/database.php
+++ config/database.php
@@ -129,13 +129,14 @@ return [
         ],
 
         'default' => [
-            'scheme' => 'tls',
+            
             'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '0'),
+            'scheme' => 'tls',
         ],
 
         'cache' => [
